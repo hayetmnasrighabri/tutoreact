7594817677be2d6b893b8a1df798dddd15048ae1
hayetmnasri <hayetmnasri@hotmail.fr>
2024-09-19 15:10:46 +0200

    textarea: les champs controlées

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { useState } from 'react'
 
 function App() {
-//const [firstName, setFirstName] = useState('john Doe')
-//const handleChange=(e)=>{
-  //setFirstName(e.target.value)
-//}
-//const handleReset=()=>{
-  //setFirstName('')
-//}
-
-const handleSubmit=(e)=>{
- e.preventDefault()
- console.log(new FormData(e.target.value))
+const[value, setValue]= useState('')
+const handleChange=(e)=>{
+  setValue(e.target.value)
 }
   return (
-    <form onSubmit={handleSubmit}>
-   <input type='text' name='firstName' defaultValue="john"/>
+    <form>
+   <textarea value={value} onChange={handleChange}/>
    <button type='submit'>Envoyer</button>
    </form>
   )
