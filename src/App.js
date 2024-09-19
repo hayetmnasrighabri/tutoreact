2555a282881edb117df67802f704015906d1275b
hayetmnasri <hayetmnasri@hotmail.fr>
2024-09-19 15:04:31 +0200

    les formulaires: les champs non controlées

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { useState } from 'react'
 
 function App() {
-const [firstName, setFirstName] = useState('john Doe')
-const handleChange=(e)=>{
-  setFirstName(e.target.value)
-}
-const handleReset=()=>{
-  setFirstName('')
+//const [firstName, setFirstName] = useState('john Doe')
+//const handleChange=(e)=>{
+  //setFirstName(e.target.value)
+//}
+//const handleReset=()=>{
+  //setFirstName('')
+//}
+
+const handleSubmit=(e)=>{
+ e.preventDefault()
+ console.log(new FormData(e.target.value))
 }
   return (
-    <form>
-   <input type='text' name='firstName' value={firstName} onChange={handleChange}/>
-    {firstName}
-   <button type='submit' onClick={handleReset}>reset</button>
+    <form onSubmit={handleSubmit}>
+   <input type='text' name='firstName' defaultValue="john"/>
+   <button type='submit'>Envoyer</button>
    </form>
   )
 }
