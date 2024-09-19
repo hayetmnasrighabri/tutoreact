da29c2a5469a55970dc9aaae1a6bbc3748dd0abd
hayetmnasri <hayetmnasri@hotmail.fr>
2024-09-19 14:48:41 +0200

    les formulaires: les champs controlées

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { useState } from 'react'
 
 function App() {
-  const [firstName, setFirstName] = useState("john Doe")
-  const handleChange=(e)=>{
-    setFirstName(e.target.value)
-  }
-  const handleReset=()=>{
-    setFirstName('')
-  }
+const [firstName, setFirstName] = useState('john Doe')
+const handleChange=(e)=>{
+  setFirstName(e.target.value)
+}
+const handleReset=()=>{
+  setFirstName('')
+}
   return (
     <form>
-      <input type='text'
-             name='firstName'
-             value={firstName}
-             onChange={handleChange}
-           />
-           {firstName}
-        <button type='submit' onClick={handleReset}>reset</button>
-    </form>
-      
-    
+   <input type='text' name='firstName' value={firstName} onChange={handleChange}/>
+    {firstName}
+   <button type='submit' onClick={handleReset}>reset</button>
+   </form>
   )
 }
 
